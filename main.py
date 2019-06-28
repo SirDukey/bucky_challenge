ecf6845b71e4e2657cc3d37245f2ee6d589dfe0a
SirDukey <michael_duke@outlook.com>
2019-06-28 15:20:16 +0200

    Adding * logic

--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 from prettytable import PrettyTable, from_csv
+import csv
+from datetime import datetime
 
 table = PrettyTable()
 table.field_names = ['id', 'name', 'price', 'expires']
 
 
+def all_params(sp):
+    """
+    All search paramaters where supplied
+    """
+    PRICE_MIN, PRICE_MAX, EXPIRES_START, EXPIRES_STOP = sp
+    with open('products.csv', 'r') as prod_csv:
+        csv_reader = csv.DictReader(prod_csv, delimiter=',')
+
+        start = datetime.strptime(EXPIRES_START, '%m/%d/%Y')
+        start = start.strftime('%m/%d/%Y')
+
+        stop = datetime.strptime(EXPIRES_STOP, '%m/%d/%Y')
+        stop = stop.strftime('%m/%d/%Y')
+
+        PRICE_MIN = float(PRICE_MIN)
+        PRICE_MAX = float(PRICE_MAX)
+
+        for row in csv_reader:
+            price = float(row['price'])
+            expires = datetime.strptime(row['expires'], '%m/%d/%Y')
+            expires = expires.strftime('%m/%d/%Y')
+            if price >= PRICE_MIN and price <= PRICE_MAX and expires >= EXPIRES_START and expires <= EXPIRES_STOP:
+                table.add_row([row['id'], row['name'], row['price'], row['expires']])
+    print(table)
+    table.clear_rows()
+
+
+def star_params(sp):
+    """
+    A * was used in the search parameters
+    """
+
+    #TODO: add logic for the * when searching
+
+    PRICE_MIN, PRICE_MAX, EXPIRES_START, EXPIRES_STOP = sp
+    with open('products.csv', 'r') as prod_csv:
+        csv_reader = csv.DictReader(prod_csv, delimiter=',')
+        for row in csv_reader:
+            pass
+
+
 def display_table(sp):
-    
     """
     Display product table
     """
-    """
-    table.add_row([1, 'First sample item', 1.23, 'JAN-01-2019'])
-    table.add_row([2, 'Second sample item', 2.34, 'JAN-02-2019'])
-    print(table)
-    table.clear_rows()
-    """
-
-    with open('products.csv', 'r') as f:
-        prod_table = from_csv(f)
-
     PRICE_MIN, PRICE_MAX, EXPIRES_START, EXPIRES_STOP = sp
-    t = prod_table.get_string(header=False, border=False, start=0, end=10)
-    print(t) 
+    if '*' in PRICE_MIN or '*' in PRICE_MAX or '*' in EXPIRES_START or '*' in EXPIRES_STOP:
+        star_params(sp)
+    else:
+        all_params(sp)
+
 
 def start():
     """
